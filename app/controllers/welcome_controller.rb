d7ff9fd4d124c87794be2c32ab987ec412b6be8d
Dewey Banks <DeweyBanks@gmail.com>
2017-09-12 10:56:42 -0400

    final solution

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,19 @@
+require 'will_paginate/array'
 class WelcomeController < ApplicationController
+
   def index
   end
 
   def search
     terms = params["terms"]
+    url = "http://www.recipepuppy.com/api/"
+    response = HTTParty.get("http://www.recipepuppy.com/api/",
+    :query => { :i => terms })
+    response = JSON.parse(response.body)["results"]
+    @results = response.paginate(:page => params[:page], :per_page => 20)
+    respond_to do |format|
+       format.json { render :json => JSON.parse(@results) }
+       format.html { render "index.html.erb" }
+    end
   end
-
-
 end
